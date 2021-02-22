60c9830ee8b4f61a03caa4385cf71e568b500fcd
Kinesys <67867993+Kinesys@users.noreply.github.com>
2021-02-23 04:43:52 +0900

    Update asm.c

--- asm/asm.c
+++ asm/asm.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/mman.h>
+#include<seccomp.h>
+#include<sys/prctl.h>
+#include<fcntl.h>
+#include<unistd.h>
 
+#define LENGTH 128
+
+void sandbox() {
+    scmp_filter_ctx ctx = seccomp_init(SCMP_ACT_KILL);
+    
+}
